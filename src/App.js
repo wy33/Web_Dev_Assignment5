a2cb4db310da66d2f8e1ac989a956f5a1b00ed6d
William Yang <yangw031@gmail.com>
2023-11-07 18:42:21 -0500

    Anime Quote API
    App.js:
    - Pulls a quote from an anime character and displays on the webpage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+export default function Gallery() {
+  const [quote, setQuote] = useState({ anime: null, character: null, quote: null });
+
+  const quoteEndpoint = 'https://animechan.xyz/api/random';
+
+
+
+  function handleGetQuoteClick() {
+    fetch(quoteEndpoint)
+        .then(response => response.json())
+        .then(json => setQuote(json))
+        .catch(error => console.error(error));
+  }
+
+  console.log(quoteEndpoint);
+
+  return (
+    <>
+      <h2>Weather & Anime Quote</h2>
+
+      <button onClick={handleGetQuoteClick}>Get Picture</button>
+
+      <h2>{quote.anime}</h2>
+      <h3>{quote.character}</h3>
+      <p>{quote.quote}</p>
+    </>
+  );
 }

0dbeea5892a11a6f13d4867f1d06927b08d8095e
HayatH <61989790+Hasnain-commits@users.noreply.github.com>
2023-11-09 19:03:58 -0500

    fixed class != className

--- src/AnimeQuote.js
+++ src/AnimeQuote.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AnimeQuote({ details }) {
 	return (
-		<div class="anime-quote">
+		<div className="anime-quote">
 			<h2>{details.anime}</h2>
 			<h3>{details.character}</h3>
 			<p>{details.quote}</p>

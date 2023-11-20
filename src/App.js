329c8e2a576a1dde3d5a0531c000ef4e677a5f05
Shadman Choudhury <choudhs2@bxscience.edu>
2023-11-19 22:29:19 -0500

    Changed the Article API to Wikipedia

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Image from "./Image.js"
 
 export default function Gallery() {
 	const [quote, setQuote] = useState({ anime: null, character: null, quote: null });
-	const [articles, setArticles] = useState([]);
+	const [article, setArticle] = useState({empty:true});
 	const [, forceUpdate] = useReducer(x => x + 1, 0);
 	const [imagedata, setImageData] = useState({ imageUrl: null });
 
 	const quoteEndpoint = 'https://animechan.xyz/api/random';
-	const articleEndpoint = "https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=7d0dd96cc13345e5ac2ea643830a09cc";
+	const articleEndpoint = "https://en.wikipedia.org/api/rest_v1/page/random/summary";
 	const imageEndpoint = 'https://api.unsplash.com/photos/random?query=newyorkcity&client_id=0-afBKY6mwQSalPCH365npBnVfjKlYNL6bIwH9Zz8PY'
 
 	function handleGetQuoteClick() {
@@ -23,7 +23,7 @@ export default function Gallery() {
 	function handleGetArticles() {
 		fetch(articleEndpoint)
 			.then(response => response.json())
-			.then(data => setArticles(data.articles))
+			.then(data => setArticle({ ...data }))
 			.catch(error => console.error(error));
 		//forceUpdate();
 	}
@@ -42,15 +42,15 @@ export default function Gallery() {
 
 	console.log(quoteEndpoint);
 	console.log(articleEndpoint);
-	console.log(articles);
+	console.log(article);
 	console.log(imageEndpoint)
 	
 	return (
 		<>
-			<ArticleList articles={articles} />
+			<ArticleList article={article} />
 			<h2>Weather & Anime Quote</h2>
 			<button onClick={handleGetQuoteClick}>Get Anime Quote</button>
-			<button onClick={handleGetArticles}>Get Articles</button>
+			<button onClick={handleGetArticles}>Get Article</button>
 			<button onClick={handleGetImage}>Get Image</button>
 			<Weather/>
 			<AnimeQuote details={quote}/>
--- src/Articles.js
+++ src/Articles.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-export default function ArticleList({ articles }) {
-	const listItems = articles.map((x) => {
+export default function ArticleList({ article }) {
+	if (article.empty) {
 		return (
-			<li><a href={x.url}>{x.title}</a></li>
-		)
-	});
+			<div class="ArticleColumn">
+				<h1>Random Article!</h1>
+				<p>Hit get Article to get a Random Wikipedia Article!</p>
+			</div>
+		);
+	}
 	return (
-		<div class="ScienceColumn">
-			<h1>Science News!</h1>
-			<ul>
-				{listItems}
-			</ul>
+		<div class="ArticleColumn">
+			<h1>Random Article!</h1>
+			<br/>
+			<a href={article.content_urls.desktop.page}><h3>{article.title}</h3></a>
+			<img src={article.thumbnail.source}/>
+			<p>{article.extract}</p>
 		</div>
 	);
 }

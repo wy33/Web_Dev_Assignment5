1371c25983e794c1b10f8a8e0a0c1f3093363e0b
William Yang <yangw031@gmail.com>
2023-11-19 21:16:13 -0500

    Found the bug, wrong variable name

--- src/features/AnimeQuote/AnimeQuote.js
+++ src/features/AnimeQuote/AnimeQuote.js
@@ -3,21 +3,21 @@ import { useSelector, useDispatch } from 'react-redux';
 // import { setQuote, selectAnimeQuote } from './AnimeQuoteSlice';
 import { setQuote } from './AnimeQuoteSlice';
 
-function AnimeQuoteData({ details }) {
-	return (
-		<div className="anime-quote-details">
-			<h2>{details.anime}</h2>
-			{/* <h3>{details.character}</h3>
-			<p>{details.quote}</p> */}
-		</div>
-	);
-}
+// function AnimeQuoteData({ details }) {
+// 	return (
+// 		<div className="anime-quote-details">
+// 			<h2>{details.anime}</h2>
+// 			{/* <h3>{details.character}</h3>
+// 			<p>{details.quote}</p> */}
+// 		</div>
+// 	);
+// }
 
 export default function AnimeQuote() {
 	// const anime = useSelector((state) => state.animeQuote.anime);
 	// const character = useSelector((state) => state.animeQuote.character);
 	// const quote = useSelector((state) => state.AnimeQuote.quote);
-	const animeQuote = useSelector((state) => state.AnimeQuote.anime);
+	const animeQuote = useSelector((state) => state.animeQuote);
 	// const animeQuote = useSelector(selectAnimeQuote);
 	const dispatch = useDispatch();
 
@@ -30,13 +30,13 @@ export default function AnimeQuote() {
 			.catch(error => console.error(error));
 		
 		// dispatch(setQuote)
-		return (
-			<div className="anime-quote-details">
-				<h2>{animeQuote}</h2>
-				{/* <h3>{details.character}</h3>
-				<p>{details.quote}</p> */}
-			</div>
-		);
+		// return (
+		// 	<div className="anime-quote-details">
+		// 		<h2>{animeQuote}</h2>
+		// 		{/* <h3>{details.character}</h3>
+		// 		<p>{details.quote}</p> */}
+		// 	</div>
+		// );
 	}
 
 	// function handleGetQuoteClick() {
@@ -60,9 +60,9 @@ export default function AnimeQuote() {
 			{/* <button onClick={() => dispatch(getQuote())}>Get Anime Quote</button> */}
 			<button onClick={getQuote}>Get Anime Quote</button>
 			{/* <AnimeQuoteData details={animeQuote}/> */}
-			{/* <h2>{details.anime}</h2>
-			<h3>{details.character}</h3>
-			<p>{details.quote}</p> */}
+			<h2>{animeQuote.anime}</h2>
+			<h3>{animeQuote.character}</h3>
+			<p>{animeQuote.quote}</p>
 		</div>
 	);
 }

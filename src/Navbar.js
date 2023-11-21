016e9caf3d2687497179c4188d04d2f57d993480
Hasnain-Commits <hasnainhayat2002@gmail.com>
2023-11-21 11:26:18 -0500

    navbar code

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+export default function NavBar() {
+    return (
+        <header className="navbar"> 
+            <nav className="nav">
+                <ul>
+                    <li><a href="">Weather</a></li>
+                    <li><a href="">Anime</a></li>
+                    <li><a href="">Article</a></li>
+                    <li><a href="">Image</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
